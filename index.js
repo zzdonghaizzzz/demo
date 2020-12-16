7ce809ff5a25321a4d79799e2de0e5884041f846
zzdonghaizzzz <75818018+zzdonghaizzzz@users.noreply.github.com>
2020-12-16 10:53:14 +0700

    Update index.js

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ app.use('/payment', paymentControl );
 
 app.listen( PORT, 
     () => {
-        console.log("\n\n--------- Server running ! PORT: ", PORT);
+        console.log("\n\n--------- Server running ! PORT: "+ PORT);
     }
 );
 
